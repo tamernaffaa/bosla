ea35873ce7d3928b376ad43eff28ed59a2da223a
tamernaffaa <31572614+tamernaffaa@users.noreply.github.com>
2025-03-31 20:32:33 +0300

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,13 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+export default nextConfig;
+module.exports = {
+  images: {
+    domains: ['ivsuscmeelotkcibziya.supabase.co'], // أضف اسم نطاق التخزين الخاص بـ Supabase هنا
+  },
+};
+
